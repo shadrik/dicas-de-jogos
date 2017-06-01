bc1e80de0a85d76eef8d260f4693fcad520818dc
shadrik <shadrikbauer@gmail.com>
2017-06-01 15:42:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
-<View style={styles.container}>
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button, Image } from 'react-native';
+import { Constants } from 'expo';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Image source={{uri: 'http://img.ibxk.com.br/2015/12/11/11135313969498.jpg'}} style={styles.container}>
         <Text style={styles.paragraph}>
           Tela de principal "dicas de jogos"como jogar blood strike e ser um profissional no blood.
           
         </Text>
         
         <Button
-          title="1-Entre no jogo"
+          title="1-Faça uma conta no Facebook ou na nossa pagina"
           onPress={this._handleButtonPress}
         />
       
         <Button
-          title="2-Clique numa sala"
+          title="2-entre no jogo"
           onPress={this._handleButtonPress}
         />
       
         <Button
-          title="3-Aperte para jogar"
+          title="3-entre numa sala"
           onPress={this._handleButtonPress}
         />
       
         <Button
-          title="4-e voce esta no jogo"
+          title="4-Aperte para jogar"
           onPress={this._handleButtonPress}
-        />
-      <Text>Obs as outras dicas vai tar no meu canal.Inscreva-se! </Text>
+          />
+          
+      <Button
+        title="5-e voce esta jogando"
+        onPress={this._handleButtonPress}
+      />
+    
+      <Text>Obs: as outras dicas vai tar no meu canal.Inscreva-se! </Text>
       
-      </View>
-      
+      </Image>
     );
   }
 }
@@ -47,4 +59,3 @@ const styles = StyleSheet.create({
     color: 'black',
   },
 });
-
